62f97957b7904ae531395c33334d30255c3984f3
Marion Dam <mariondam@macbookair.home>
2025-10-01 22:51:20 +0200

    Small updates

--- dutch_energy_markets/get_dutch_energy_market_data.py
+++ dutch_energy_markets/get_dutch_energy_market_data.py
@@ -7,20 +7,12 @@ get_afrr_df(start_date, end_date, entsoe_client) :
     quarterly aFRR dispatch, from entsoe, using entsoe-py client
 get_dayahead_df(start_date, end_date, entsoe_client) : 
     hourly EPEX day-ahead prices from entsoe, using entsoe-py client
-get_minute_df(start_date, end_date) : 
-    imbalance data per minute from Tennet
-get_vp_df(start_date, end_date) : 
-    quarterly VerrekenPrijzen from Tennet
 '''
 
 import pandas as pd
 import requests
 from datetime import datetime
 
-def _date_tennet_format(input_date):
-    date_object = datetime.strptime(input_date, '%Y%m%d')
-    formatted_date = date_object.strftime('%d-%m-%Y')
-    return formatted_date
     
 def get_afrr_df(start_date, end_date, entsoe_client):
     # Use ENTSOE-py to fetch aFRR data (both amount and prices)
@@ -59,58 +51,3 @@ def get_dayahead_df(start_date, end_date, entsoe_client):
 
     return dayahead_df
 
-
-def get_minute_df(start_date, end_date):
-    # Fetch data from TenneT
-    url = f"http://www.tennet.org/bedrijfsvoering/ExporteerData.aspx"
-
-    datatype = 'balansdeltaIGCC'
-
-    params = {
-            'exporttype': datatype,
-            'format': 'xml',
-            'datefrom': _date_tennet_format(start_date),
-            'dateto': _date_tennet_format(end_date),
-            'submit': '1'
-        }
-
-    response = requests.get(url, params = params)
-    minute_df = pd.read_xml(response.content, xpath='//Record')
-
-    # give column names suffix _1m for minute data
-    cols = list(minute_df.columns)
-    cols = [str.lower(c) + '_1m' for c in cols]
-    minute_df.columns = cols
-
-    # make datetime index
-    minute_df['datetime'] = pd.to_datetime(minute_df['date_1m'] + ' ' + minute_df['time_1m'], format = '%d-%m-%Y %H:%M')
-    minute_df = minute_df.set_index('datetime').tz_localize('Europe/Amsterdam')
-
-    return minute_df
-
-
-def get_vp_df(start_date, end_date):
-    datatype = 'verrekenprijzen'
-    params = {
-            'exporttype': datatype,
-            'format': 'xml',
-            'datefrom': _date_tennet_format(start_date),
-            'dateto': _date_tennet_format(end_date),
-            'submit': '1'
-        }
-
-    url = f"http://www.tennet.org/bedrijfsvoering/ExporteerData.aspx"
-    response = requests.get(url, params = params)
-    vp_df = pd.read_xml(response.content, xpath='//Record')
-
-    # give column names suffix _vp for VerrekenPrijzen (imbalance settlement)
-    cols = list(vp_df.columns)
-    cols = [str.lower(c) for c in cols]
-    vp_df.columns = cols
-
-    # create datetime index
-    vp_df['datetime'] = vp_df['date'] + ' ' + vp_df['period_from']
-    vp_df['datetime'] = pd.to_datetime(vp_df['datetime'], format = '%d-%m-%Y %H:%M')
-    vp_df = vp_df.set_index('datetime').tz_localize('Europe/Amsterdam', ambiguous= 'infer')
-    
-    return vp_df
---
+++ dutch_energy_markets/readme.md
@@ -0,0 +1,11 @@
+# Data sources Dutch electricity markets
+
+Data from ENTSOE using entsoe-py (see Example-dayahead_aFRR.ipynb):
+- Day ahead electricity prices (per hour) 
+- aFRR activations and prices (per 15 min) 
+
+View and download day-ahead electricity prices for countries in Europe here: [Transparency platform](https://newtransparency.entsoe.eu/market/energyPrices).
+
+Data from Tennet can be downloaded here: [Download Page Transparency](https://www.tennet.eu/nl-en/node/3479). For example:
+- (Current) imbalance
+- Imbalance settlement prices (per 15 min)
